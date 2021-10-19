abe6381521c440067d53352c36c8900a61cc3a76
Stella C <Stella-designs@pm.me>
2021-10-19 17:25:41 -0500

    Add files via upload

---
+++ scripts/forecast.js
@@ -0,0 +1,27 @@
+const key = '1x3jOLAJ8q6fh0FykouKxXhfZixC8Kkk';
+
+// get weather information
+const getWeather = async (id) => {
+
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
+
+//  get city information
+
+const getCity = async (city) => {
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+    
+    return data[0];
+};
+
